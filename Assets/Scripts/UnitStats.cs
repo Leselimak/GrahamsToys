ed89c5a5187e927ace3b5004f1e2e63d55bbad89
Shameeg <39-13013@sae.edu.za>
2022-05-11 12:50:59 +0200

    Fixed Background for game and ui and added a Unit Script
    Unit scripts are used for health and damage of characters

---
+++ Assets/Scripts/UnitStats.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : MonoBehaviour
+{
+    public int damage;
+    public int healthPoints;
+    public int currentHealth;
+}
