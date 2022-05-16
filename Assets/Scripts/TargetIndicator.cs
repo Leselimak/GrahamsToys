55b32a35577405cd499637fb479a2149b335a91d
Leselimak <39-13015@sae.edu.za>
2022-05-16 19:43:23 +0200

    Target indicator added
    Target indicator

---
+++ Assets/Scripts/TargetIndicator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetIndicator : MonoBehaviour
+{
+    public Transform Target;
+    public float HideDistance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Target.position - transform.position;
+
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); 
+
+        if(dir.magnitude < HideDistance)
+        {
+            SetChildrenActive(false);
+        }
+        else
+        {
+            SetChildrenActive(true);
+        }
+    }
+
+    void SetChildrenActive(bool value)
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(value);
+        }
+    }
+}
